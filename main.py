de344e4a66612aaf803268d2ff62e9c1e17118da
willadomm <wdomm@oberlin.edu>
2025-12-27 16:18:09 -0800

    finished!

---
+++ main.py
@@ -0,0 +1,51 @@
+import json
+import pandas
+
+
+def main():
+    file_path = 'template.json'
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    
+    tableformatteddict = createwinsdictionary(data)
+
+    dataframe = pandas.DataFrame(tableformatteddict)
+
+    new_index_labels = createnewindexlabels(data)
+
+    dataframe.index = new_index_labels
+
+    print(dataframe.transpose())
+
+
+        
+        
+            
+def createwinsdictionary(data):
+    teamwithwinsdict = {}
+    
+    i = 0
+
+    for team in data:
+        wins = []
+        for teamrecord in data[team].values():
+            wins.append(teamrecord["W"])
+        wins.insert(i, "-")
+        teamwithwinsdict[team] = wins
+        i += 1
+    
+
+    return teamwithwinsdict
+
+def createnewindexlabels(data):
+    teams = []
+    for team in data:
+        teams.append(team)
+    
+    return teams
+
+
+    
+
+
+main()
